452039ba50741c743a05ec899838e45218043ff3
buwaneka-oshajith <buwaneka.oshajith@gmail.com>
2024-04-02 19:18:00 +0530

    New brand swipper

--- assets/js/main.js
+++ assets/js/main.js
@@ -588,5 +588,24 @@ AOS.init({
     duration: 1500,
 });
 
+/*--
+    Swipper Brand
+-----------------------------------*/
+
+var brandSlider = new Swiper('.brand-slider .swiper-container', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+
 
 
